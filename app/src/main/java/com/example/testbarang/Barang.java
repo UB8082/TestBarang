dae897867439b0059dcf04815d914fc8a8ca747c
Lucky Ahmad Ardiansyah <lucky.ahmad.ft18@mail.umy.ac.id>
2020-05-18 21:39:29 +0700

    test

---
+++ app/src/main/java/com/example/testbarang/Barang.java
@@ -0,0 +1,44 @@
+package com.example.testbarang;
+import java.io.Serializable;
+import java.security.PrivateKey;
+
+public class Barang implements Serializable{
+    private String kode;
+    private String nama;
+
+    public Barang()
+    {
+
+    }
+
+    public String getKode()
+    {
+    return kode;
+    }
+
+    public void setKode(String kd)
+    {
+        this.kode = kd;
+    }
+
+    public String getNama()
+    {
+        return nama;
+    }
+
+    public  void setNama(String nama)
+    {
+        this.nama = nama;
+    }
+    @Override
+    public String toString()
+    {
+        return " "+kode+"\n"+" "+nama;
+    }
+    public Barang(String kd, String nm)
+    {
+        kode = kd;
+        nama = nm;
+    }
+
+}
